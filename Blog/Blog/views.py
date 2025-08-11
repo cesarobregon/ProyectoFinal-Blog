d6eaad8c13a356522fee1591be4e53db96fe219e
cesarobregon <cesarobregon66@gmail.com>
2025-08-11 15:56:45 -0300

    botones 'editar' y 'eliminar' agregados a los articulos

--- Blog/Blog/views.py
+++ Blog/Blog/views.py
@@ -8,8 +8,7 @@ from apps.articulos.models import Articulo
 def Home(request):       
     
     ultimos_articulos = Articulo.objects.order_by('-creado')[:3]  # Los 3 más recientes
+    es_moderador = request.user.is_authenticated and request.user.groups.filter(name='Moderador').exists()
     
-    return render(request, 'home.html', {'ultimos_articulos': ultimos_articulos})
-
-
+    return render(request, 'home.html', {'ultimos_articulos': ultimos_articulos,'es_moderador': es_moderador})
 
